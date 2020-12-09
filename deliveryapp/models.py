b95a9493adadf959b2cc3e382091d5d50deab50f
Charles maina <muragemaina09@gmail.com>
2020-12-09 14:59:11 +0300

    added app

--- deliveryapp/models.py
+++ deliveryapp/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 
-class Business(models.Model):
-    name = models.CharField(max_length=40)
-    logo = models.ImageField(upload_to='logo', blank=True)
-    phone = models.CharField(max_length=10)
-    
+# Create your models here.
